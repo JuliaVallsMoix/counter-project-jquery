69a5f484b7324cc2279f8e9dee93123bf59892d0
Júlia Valls Moix <juliavalls20@gmail.com>
2023-05-09 11:11:55 +0200

    Done

--- js/app.js
+++ js/app.js
@@ -3,4 +3,30 @@ let counter = 0;
 $(document).ready(function () {
     console.log("DOM cargado! Implementa tu solución a partir de esta línea.");
 
+    $('.nextBtn').click(function () {
+        counter++;
+        $('#counter').text(counter);
+        colors();
+
+    });
+    $('.prevBtn').click(function () {
+        counter--;
+        $('#counter').text(counter);
+        colors();
+    });
+
+    function colors() {
+        if (counter > 0) {
+            $('#counter').addClass('positivo');
+        } else if (counter < 0) {
+            $('#counter').addClass('negativo');
+        } else if (counter === 0) {
+            $('#counter').removeClass('positivo');
+            $('#counter').removeClass('negativo');
+        };
+    };
+    
+
+    
+
 });
